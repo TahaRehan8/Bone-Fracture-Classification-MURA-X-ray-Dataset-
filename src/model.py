24ebb7b754757c84d4c6931eb8572c51207027db
Taha Rehan <taharehan84@gmail.com>
2026-07-16 04:12:25 +0500

    Patched Dense layer deserialization to support loading Keras 3 models in Keras 2 environments like Render

--- src/model.py
+++ src/model.py
@@ -136,7 +136,17 @@ def load_model(
             f"Train the model first or download the weights."
         )
 
-    model = keras.models.load_model(str(weights_file))
+    class SafeDense(layers.Dense):
+        """Wrapper to ignore Keras 3-specific arguments when loading in Keras 2 environments."""
+        def __init__(self, **kwargs):
+            kwargs.pop('quantization_config', None)
+            super().__init__(**kwargs)
+
+    model = keras.models.load_model(
+        str(weights_file),
+        custom_objects={"Dense": SafeDense},
+        compile=False
+    )
 
     logger.info(
         "Loaded '%s' from '%s'",
